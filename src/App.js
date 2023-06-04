5561aefd4551822e33f2b5b37e6f5b9e026eb2cb
Katherine Zannis <katherinezannis@gmail.com>
2023-06-04 18:37:30 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Katherine Zannis and is{" "}
-        <a href="https://github.com/zaki-san/react-weather-app">
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Katherine Zannis and is{" "}
+          <a href="https://github.com/zaki-san/react-weather-app">
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
