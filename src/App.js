9ca719f118bfaac7162265f65082cd4849670756
Katherine Zannis <katherinezannis@gmail.com>
2023-06-03 22:16:00 -0500

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Katherine Zannis and is{" "}
+        <a href="https://github.com/zaki-san/react-weather-app">
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
