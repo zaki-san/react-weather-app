8e2f724de0ab21ed625d07d6cd5472a6f6918c7b
Katherine Zannis <katherinezannis@gmail.com>
2023-06-04 18:59:10 -0500

    Implemented the weather app structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Katherine Zannis and is{" "}
           <a href="https://github.com/zaki-san/react-weather-app">
-            open-sourced on Github.
+            open-sourced on Github
           </a>
+          .
         </footer>
       </div>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Cloudly</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="Cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 46%</li>
+            <li>Precipitation: 11km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
