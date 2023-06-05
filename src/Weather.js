31abf5593ee168e27ea8dcab856bccc38cf67249
Katherine Zannis <katherinezannis@gmail.com>
2023-06-05 00:42:24 -0500

    Added WeatherIcon component

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       city: response.data.name,
     });
   }
