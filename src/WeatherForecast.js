6ec7b4dd3e51714b302394f901ff59bbb6a14142
Katherine Zannis <katherinezannis@gmail.com>
2023-06-10 18:51:15 -0500

    Integrated coordinates from the API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponde);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
